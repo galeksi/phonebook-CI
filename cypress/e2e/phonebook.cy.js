883bcb7b58702392aef6d90da9e2d2fb880cf463
Aleksi Rendel <aleksi.rendel@gmail.com>
2023-04-05 19:01:57 +0300

    Added fly deployment

--- cypress/e2e/phonebook.cy.js
+++ cypress/e2e/phonebook.cy.js
@@ -4,4 +4,8 @@ describe('Phonebook', function () {
     cy.contains('Phonebook');
     cy.contains('Aleksi Rendel');
   });
+  it('Names are loaded', function () {
+    cy.visit('http://localhost:3001');
+    cy.contains('Aleksi Rendel');
+  });
 });
