ebf1bbf16232dac3f1f2be27372bf3df2d175222
Aleksi Rendel <aleksi.rendel@gmail.com>
2023-04-05 15:40:26 +0300

    Added cypress

--- frontend/src/Services/persons.js
+++ frontend/src/Services/persons.js
@@ -21,4 +21,5 @@ const destroy = (id) => {
   return request.then((response) => response.data);
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, create, destroy, update };
