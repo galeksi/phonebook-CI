fc572a4e8a29b3a018beddc214d65bed62a7298d
Aleksi Rendel <aleksi.rendel@gmail.com>
2023-04-05 19:05:23 +0300

    Corrected deployment workflow

--- cypress/e2e/phonebook.cy.js
+++ cypress/e2e/phonebook.cy.js
@@ -2,7 +2,6 @@ describe('Phonebook', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:3001');
     cy.contains('Phonebook');
-    cy.contains('Aleksi Rendel');
   });
   it('Names are loaded', function () {
     cy.visit('http://localhost:3001');
